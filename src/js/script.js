708bd7e8dd35acccad88131c1119c1ab5d776fcc
tendernesc <sanasin22153@mail.ru>
2024-02-09 22:06:11 +0300

    perf (Wildberries) added new styles and card image

--- src/js/script.js
+++ src/js/script.js
@@ -28,11 +28,52 @@ const wrapper = createElement('div', 'main-wrapper', main,'');
 
 //Слайдер
 const slider = createElement('div', 'main-wrapper-slider', wrapper, '');
+const sliderContainer = createElement('div', 'main-wrapper-slider-container',slider, '');
 
 
 
-const textInfo = createElement('h4', 'main-wrapper__info',wrapper, 'Хиты продаж')
+
+const textInfo = createElement('h4', 'main-wrapper__info',wrapper, 'Хиты продаж');
+
+
+
+
 
 
 //Карточки
 const cardsWrapper = createElement('div','main-wrapper-cards', wrapper,'');
+
+
+async function getCards() {
+  const response = await fetch('https://65c28052f7e6ea59682b76ff.mockapi.io/id/Wildberies');
+  const result = await response.json();
+  return result;
+}
+
+async function printCards(cards) {
+  for (let i = 0; i < 8 ; i++) {
+    const cardContainer = createElement('div', 'main-wrapper-cards-container', cardsWrapper, '');
+    const card = cards[i];
+    const randomParam = Math.floor(Math.random() * (1000 - 1 + 1) + 1);
+
+    const imageCard = createElement('img', 'main-wrapper-cards-container__img', cardContainer, '');
+    imageCard.src = card.picture + `?random=${randomParam}`;
+    imageCard.alt = card.title;
+
+    // const cardIncrease = createElement('button', 'main')
+    // const cardВecrease
+    const cardTitle = createElement('h3', 'main-wrapper-cards-container__title', cardContainer, `${card.title}`);
+    const cardPrice = createElement('h4', 'main-wrapper-cards-container__price', cardContainer, `Цена: ${card.price}`);
+
+    imageCard.addEventListener('click', () => {
+      imageCard.style.transform = 'scale(1.5)'; // Увеличиваем на 10% (можете настроить по вашему вкусу)
+      imageCard.style.transition = 'transform 0.3s'; // Добавляем плавность анимации
+    });
+
+    
+  }
+}
+
+getCards().then(data => printCards(data));
+
+
